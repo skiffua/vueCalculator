217da33a7b9ce967197eeb9635223ac007898859
Oleh <oleh_mushora@ukr.net>
2019-10-16 20:52:29 +0300

    add_header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">    
+    <Header/>
+    <EnvironmentBlock />
   </div>
 </template>
 
 <script>
+import Header from './components/Header'
+import EnvironmentBlock from './components/EnvironmentBlock'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
+    Header,
+    EnvironmentBlock,
     HelloWorld
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ 
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,81 @@
+<template>
+  <header>
+    <div class="header_block">TheBestCalculator</div>
+    <div class="header_block menu_block">
+      <ul class="menu">
+        <li class="active_link">
+          <a href="#" >Main</a>
+        </li>
+        <li>
+          <a href="#">History</a>
+        </li>
+      </ul>
+    </div>
+    <div class="header_block">
+      <div class="user_name">
+        Funny user
+        </div>
+      <div class="logout_element">
+      <a href="#"><i class="fas fa-power-off"></i> </a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  background-color: #373737;
+  height: 60px;
+  color: #ff7d20;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 20px;
+  padding-right: 20px;
+}
+.menu_block {
+  width: 120px;
+}
+.menu {
+  padding-left: 0px;
+  display: flex;
+  justify-content: space-between;
+}
+li {
+  display: inline-block;
+  padding-bottom: 6px;
+  padding-top: 6px;
+}
+.active_link {
+  border-bottom: 1px solid;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+.user_name {
+  color: #ffffff;
+  padding-right: 15px;
+  display: inline-block;
+  border-right: 1px solid #ffffff;
+}
+.logout_element {
+  display: inline-block;
+  padding-left: 15px;
+  text-align: center;
+}
+</style>
